e159373f45ca26ca4aa8ce893c26468d0392e012
Y.V <5196666qwe@gmail.com>
2025-09-03 18:12:46 +0800

    fix: 更新版本号至 1.2.1，并修正 manifest.json 路径

--- scripts/bump-version.js
+++ scripts/bump-version.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 const pkg          = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-const manifestPath = 'extension/manifest.json';
+const manifestPath = './manifest.json';
 const manifest     = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
 manifest.version   = pkg.version;
 fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
